e7247d15ce045e24df197fbf31c3cfb774bf267a
Nata Nael <natanael280198@gmail.com>
2022-06-13 13:58:56 +0700

    Streamlined codes in app.js

--- src/App.js
+++ src/App.js
@@ -8,42 +8,43 @@ import Nav from "./Nav";
 function App() {
   return (
     <div className = "app" >
-    <Nav/> 
-    <Banner/>
-    <Row title = "NETFLIX ORIGINALS"
-    fetchUrl = {
-      requests.fetchNetflixOriginals
-    }
-    isLargeRow >
-    </Row> <
-    Row title = "Trending Now"
-    fetchUrl = {
-      requests.fetchTrending
-    } > </Row> 
-    <Row title = "Top Rated"
-    fetchUrl = {
-      requests.fetchTopRated
-    } > </Row> <
-    Row title = "Action Movies"
-    fetchUrl = {
-      requests.fetchActionMovies
-    } > </Row> 
-    <Row title = "Comedy Movies"
-    fetchUrl = {
-      requests.fetchComedyMovies
-    } > </Row> 
-    <Row title = "Romance Movies"
-    fetchUrl = {
-      requests.fetchRomanceMovies
-    }> </Row> 
-    <Row title = "Horror Movies"
-    fetchUrl = {
-      requests.fetchHorrorMovies
-    } > </Row> 
-    <Row title = "Trending Now"
-    fetchUrl = {
-      requests.fetchTrending
-    } > </Row> 
+      <Nav/> 
+      <Banner/>
+      <Row
+        title = "NETFLIX ORIGINALS"
+        fetchUrl = {
+          requests.fetchNetflixOriginals
+        }
+        isLargeRow
+      ></Row>
+
+      <Row
+        title = "Trending Now"
+        fetchUrl = {
+          requests.fetchTrending
+        }
+      ></Row> 
+
+      <Row
+        title = "Top Rated"
+        fetchUrl = {
+          requests.fetchTopRated
+        }
+      ></Row>
+
+      <Row
+        title = "Action Movies"
+        fetchUrl = {
+          requests.fetchActionMovies
+        }
+      ></Row>
+
+      <Row
+        title = "Comedy Movies"
+        fetchUrl = {
+          requests.fetchComedyMovies
+        }
+      ></Row>
     </div>
   );
 }
